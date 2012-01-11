99eef601d9a49d055f44f09ddf7920e9b7bbc70f
Alex Angelini <alex.louis.angelini@gmail.com>
2012-01-11 16:59:53 -0500

    Initial commit

---
+++ example/resources/users/users.js
@@ -0,0 +1,15 @@
+var Users = {
+  Resource: {
+    get: function(uid, callback) {
+      callback(null, {uid: uid, query: this.query});
+    }
+  },
+
+  Collection: {
+    get: function(callback) {
+      callback(null, {all: 'users'});
+    }
+  }
+}
+
+module.exports = Users;
---
+++ example/server.js
@@ -0,0 +1,9 @@
+var connect = require('connect');
+var resty = require('../resty');
+
+var app = connect.createServer();
+app.use(connect.query());
+app.use(resty(__dirname + '/resources'));
+app.listen(3000);
+
+console.log('Server listening on port 3000');
