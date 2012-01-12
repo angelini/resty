c03df8be693e64fbcda097064a3e9682c9743965
Alex Angelini <alex.louis.angelini@gmail.com>
2012-01-12 12:04:20 -0500

    Export utility functions for testing

--- example/server.js
+++ example/server.js
@@ -3,7 +3,7 @@ var resty = require('../resty');
 
 var app = connect.createServer();
 app.use(connect.query());
-app.use(resty(__dirname + '/resources'));
+app.use(resty.middleware(__dirname + '/resources'));
 app.listen(3000);
 
 console.log('Server listening on port 3000');
